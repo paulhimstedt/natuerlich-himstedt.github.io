381d4eb665afd3b75c8b9becc1aeede7c8f768c0
paulhimstedt <71269641+paulhimstedt@users.noreply.github.com>
2022-04-08 00:00:06 +0200

    V002
    Logic for more button for first section

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-//function goToByScroll(id) {
-//    // Reove "link" from the ID
-//    id = id.replace("link", "");
-//    // Scroll
-//    $('html,body').animate({
-//            scrollTop: $("#" + id).offset().top
-//        },
-//        'slow');
-//}
-//
-//$("#menu-container > ul > li > a").click(function(e) {
-//    // Prevent a page reload when a link is pressed
-//    console.log("zacjk")
-//    e.preventDefault();
-//    // Call the scroll function
-//    goToByScroll($(this).attr("id"));
-//});
+function section_one_more() {
+    document.getElementById("moresection1_p").innerHTML = "<p class='py-2 text-2xl'>addd itional info 1 natürlich“ tauchen im</p>";
+    document.getElementById("moresection1").setAttribute('onclick', 'section_one_less()')
+    document.getElementById("moresection1").innerHTML = "Einklappen";
+}
 
-//$(document).ready(function() {
-//    $("#beratunglink").click(function() {
-//        console.log("klickklacjk")
-//        var x = $("#beratung").position(); //gets the position of the div element...
-//        window.scrollTo(x.left, x.top); //window.scrollTo() scrolls the page upto certain position....
-//        //it takes 2 parameters : (x axis cordinate, y axis cordinate);
-//    });
-//});
+function section_one_less() {
+    document.getElementById("moresection1_p").innerHTML = "";
+    document.getElementById("moresection1").innerHTML = "Mehr";
+    document.getElementById("moresection1").setAttribute('onclick', 'section_one_more()')
+}
